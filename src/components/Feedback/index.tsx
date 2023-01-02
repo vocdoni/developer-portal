917082661a0a372ea795efdbff14f41e1d49aae3
selankon <selankon@selankon.xyz>
2023-01-02 13:20:54 +0100

    Style feedback component

--- src/components/Feedback/index.tsx
+++ src/components/Feedback/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import "@ramseyinhouse/feedback-component/dist/index.umd.js"
+import styles from "./styles.module.css"
 
 export default function Feedback({ id } : { id: string }) {
     return (
-        <div>
+        <div className="feedback">
             {/*@ts-ignore*/}
             <feedback-component>
                 <span slot="cta">Was this helpful?</span>
                 <span slot="confirmation">Thanks for your feedback! </span>
                 {/*Thumbs up*/}
                 <span slot="option-icon:0" className={"plausible-event-name=feedback-component-vote-up plausible-event-page="+id}>
-                    <i className="far fa-thumbs-up"></i>
+                    <i className="far fa-thumbs-up fa-2x"></i>
                 </span>
                 {/*Thumbs down*/}
                 <span slot="option-icon:1" className={"plausible-event-name=feedback-component-vote-down plausible-event-page="+id}>
-                    <i className="far fa-thumbs-down"></i>
+                    <i className="far fa-thumbs-down fa-2x"></i>
                 </span>
                 {/*@ts-ignore*/}
             </feedback-component>
