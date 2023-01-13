1f7b5478761ea18cbf26c709d09a7c403a090648
selankon <selankon@selankon.xyz>
2023-01-13 14:44:23 +0100

    Implement codeblock window

---
+++ src/components/TypewriterCodeBlock/fakeWindow.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const FakeWindow = ({className = '', children}) => {
+    return <Window className={className}>
+        <WindowControls>
+            <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
+                <g fill="none" fill-rule="evenodd" transform="translate(1 1)">
+                    <circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"></circle>
+                    <circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"></circle>
+                    <circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"></circle>
+                </g>
+            </svg>
+        </WindowControls>
+        <WindowContent>
+            {children}
+        </WindowContent>
+    </Window>
+}
+
+const Window = styled.div.attrs({className: "rounded drop-shadow-lg"})`
+  background-color: #282A36;
+`;
+
+const WindowControls = styled.div.attrs({className: "pl-5 py-1"})`
+`;
+
+const WindowContent = styled.div.attrs({className: ""})`
+
+`;
