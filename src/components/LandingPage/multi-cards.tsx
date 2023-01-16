af03b95059993d3b6e18f29c5decb21b0e993941
selankon <selankon@selankon.xyz>
2023-01-16 13:51:06 +0100

    WIP multi cards section

---
+++ src/components/LandingPage/multi-cards.tsx
@@ -0,0 +1,129 @@
+import styled from 'styled-components';
+import React from "react";
+import { FiArrowUpRight } from "react-icons/fi";
+
+
+interface ICard {
+    title: string
+    body: string
+    linkText: string
+    href: string
+}
+
+const cards: ICard[] = [
+    {
+        title: "GETTING STARTED",
+        body: "Deploy an enterprise-ready instance of Hasura in minutes using your container platform of choice.",
+        href: "blah",
+        linkText: "See Hasura Enterprise"
+    },
+    {
+        title: "GETTING STARTED",
+        body: "Deploy an enterprise-ready instance of Hasura in minutes using your container platform of choice.",
+        href: "blah",
+        linkText: "See Hasura Enterprise"
+    },
+    {
+        title: "GETTING STARTED",
+        body: "Deploy an enterprise-ready instance of Hasura in minutes using your container platform of choice.",
+        href: "blah",
+        linkText: "See Hasura Enterprise"
+    },
+    {
+        title: "GETTING STARTED",
+        body: "Deploy an enterprise-ready instance of Hasura in minutes using your container platform of choice.",
+        href: "blah",
+        linkText: "See Hasura Enterprise"
+    },
+    {
+        title: "GETTING STARTED",
+        body: "Deploy an enterprise-ready instance of Hasura in minutes using your container platform of choice.",
+        href: "blah",
+        linkText: "See Hasura Enterprise"
+    },
+    {
+        title: "GETTING STARTED",
+        body: "Deploy an enterprise-ready instance of Hasura in minutes using your container platform of choice.",
+        href: "https://link.to",
+        linkText: "See Hasura Enterprise"
+    },
+]
+
+
+const Card = ({title = '', body='', linkText = '', href = '', n }: { n: number } & ICard) => {
+    return (
+        <CardBox>
+            <a href={href}>
+                <CardTitle>{n}{title}</CardTitle>
+            </a>
+            <CardBody>{body}</CardBody>
+            <CardLink href={href}>
+                {linkText}
+                <FiArrowUpRight />
+            </CardLink>
+        </CardBox>
+    )
+}
+
+export default function MultiCards(): JSX.Element {
+    return (
+        <MainWrapper>
+            <Title>Create and integrate<br/> applications quickly and easily.</Title>
+            <CardsWrapper>
+                {cards.map((card, i) => (
+                    <Card
+                        key={i}
+                        n={i}
+                        title={card.title}
+                        body={card.body}
+                        linkText={card.linkText}
+                        href={card.href}
+                    />
+                ))}
+            </CardsWrapper>
+        </MainWrapper>
+    )
+}
+
+const MainWrapper = styled.div.attrs({
+    className: 'flex flex-col'
+})``;
+
+const Title = styled.h1`
+  font-size: 36px;
+  line-height: 125%;
+  color: #000000;
+  font-weight: bold;
+`;
+
+const CardsWrapper = styled.div.attrs({
+    className: 'grid md:grid-cols-3 gap-2'
+})``;
+
+const CardBox = styled.div.attrs({
+    className: 'max-w-sm p-5 bg-white border border-gray-200 shadow-md'
+})`
+  background: linear-gradient(202.06deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 100%);
+  box-shadow: inset -1px 1px 4px rgba(255, 255, 255, 0.25);
+  filter: drop-shadow(-2px 2px 16px rgba(0, 0, 0, 0.1));
+  backdrop-filter: blur(4px);
+  border-radius: 8px;
+`;
+
+const CardTitle = styled.h5.attrs({
+    className: 'mb-2 text-black font-bold'
+})``;
+
+const CardBody = styled.p.attrs({
+    className: 'mb-3 font-normal text-sm text-black'
+})``;
+
+const CardLink = styled.a.attrs({
+    className: 'inline-flex items-center hover:underline text-black pt-7 place-items-end'
+})`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 125%;
+  
+`;
