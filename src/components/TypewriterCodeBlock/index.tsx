f3740ebf2010b0c9d376fbd99ef1edd99f492075
selankon <selankon@selankon.xyz>
2023-03-09 09:07:38 +0100

    Fix children state update

--- src/components/TypewriterCodeBlock/index.tsx
+++ src/components/TypewriterCodeBlock/index.tsx
@@ -1,26 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import CodeBlock from '@theme/CodeBlock';
 
 const TypewriterCodeBlock = (
     { language = '', title = '', timeout = 50, className = '', children }
 ) => {
     const [code, setCode] = useState('');
+    const childrenRef = useRef(children);
 
     useEffect(() => {
         if (timeout > 0) {
             let index = 0;
             const type = () => {
-                if (index < children.length) {
-                    setCode(code => code + children.charAt(index));
+                if(index === 0) {
+                    setCode(children.charAt(index));
                     index++;
                     setTimeout(type, timeout);
+                }
+                else if (index < children.length) {
+                    // Check if children has changed before updating code
+                    if (childrenRef.current === children) {
+                        setCode(code => code + children.charAt(index));
+                        index++;
+                        setTimeout(type, timeout);
+                    }
                 }
             };
             type();
         } else {
             setCode(children );
         }
-    }, []);
+        // Update the childrenRef whenever children changes
+        childrenRef.current = children;
+    }, [children]);
 
     return (
         <CodeBlock
