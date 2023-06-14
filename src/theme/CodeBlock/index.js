37d5b09bf2fb23e2a0211bcf1787c1a5946fcf64
selankon <selankon@selankon.xyz>
2023-06-14 12:40:40 +0200

    Add chakra components

---
+++ src/theme/CodeBlock/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Playground from '@theme/Playground';
+import ReactLiveScope from '@theme/ReactLiveScope';
+import CodeBlock from '@theme-init/CodeBlock';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { theme } from '@vocdoni/chakra-components';
+
+const withLiveEditor = (Component) => {
+    function WrappedComponent(props) {
+        if (props.live) {
+            // @ts-expect-error: we have deliberately widened the type of language prop
+            return (
+                <ChakraProvider theme={extendTheme(theme)} resetCSS={false}>
+                    <Playground scope={ReactLiveScope} {...props} />
+                </ChakraProvider>
+            )
+        }
+        return <Component {...props} />;
+    }
+    return WrappedComponent;
+};
+export default withLiveEditor(CodeBlock);
---
+++ src/theme/ReactLiveScope/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Image } from '@vocdoni/chakra-components';
+
+// Add react-live imports you need here
+const ReactLiveScope = {
+  React,
+  ...React,
+  Image
+};
+export default ReactLiveScope;
