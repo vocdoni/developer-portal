81883507f53ec09490be158e129e3fe84468e82a
selankon <selankon@selankon.xyz>
2023-06-23 12:36:00 +0200

    Start writting docs

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Image } from '@vocdoni/chakra-components';
+import { Image,
+  ElectionProvider,
+  ElectionHeader,
+  ElectionTitle,
+  ElectionSchedule,
+  ElectionStatusBadge,
+  ElectionDescription,
+  ElectionQuestions,
+  OrganizationProvider,
+  OrganizationAvatar,
+  OrganizationDescription,
+  OrganizationHeader,
+  OrganizationName,
+  Organization,
+} from '@vocdoni/chakra-components';
 
 // Add react-live imports you need here
 const ReactLiveScope = {
   React,
   ...React,
-  Image
+  Image,
+  ElectionProvider,
+  ElectionHeader,
+  ElectionTitle,
+  ElectionSchedule,
+  ElectionStatusBadge,
+  ElectionDescription,
+  ElectionQuestions,
+  OrganizationProvider,
+  OrganizationAvatar,
+  OrganizationDescription,
+  OrganizationHeader,
+  OrganizationName,
+  Organization,
 };
 export default ReactLiveScope;
